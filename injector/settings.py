7d1a0c977473fa446eb3f13e1ebad8fe2283b315
Mark Gacoka Mbui <markgacoka@gmail.com>
2022-04-02 07:49:49 +0100

    Add base HTML

--- injector/settings.py
+++ injector/settings.py
@@ -77,6 +77,8 @@ USE_I18N = True
 USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'templates/static')]
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 django_heroku.settings(locals())
